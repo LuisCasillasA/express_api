247f66a0f8094eb8d8c541ea9458b2458de82ec9
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-09 11:48:45 -0500

    Get with id

--- app.js
+++ app.js
@@ -6,14 +6,11 @@ const app = express()
 const port = 3000
 
 //HTTP 200
-app.get('/v1/explorers', (req, res) =>{
+app.get('/v1/explorers/:id', (req, res) =>{
     console.log(`Api Explorers GET ALL request ${new Date()}`)
-    const explorer1 = {id: 1, name: 'Explorer1'}
-    const explorer2 = {id: 2, name: 'Explorer2'}
-    const explorer3 = {id: 3, name: 'Explorer3'}
-    const explorer4 = {id: 4, name: 'Explorer4'}
-    const explorers = [explorer1, explorer2, explorer3, explorer4]
-    res.status(200).json(explorers)
+    console.log(`Getting explorer with ID ${req.params.id}`)
+    const explorer = {id: 1, name: 'Explorer1'}
+    res.status(200).json(explorer)
 })
 
 //Start this app
