d07a0d905117cd788aa5fe1497cbecc519aa0bfe
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-09 12:28:04 -0500

    Put request

--- app.js
+++ app.js
@@ -13,6 +13,19 @@ app.get('/v1/explorers/:id', (req, res) =>{
     res.status(200).json(explorer)
 })
 
+app.post('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requestBody = req.body //Client params
+    res.status(201).json({message: 'Created'})
+})
+
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`)
+    console.log(`Update explorers with id ${req.params.id}`)
+    const requestBody = req.body //Client params
+    res.status(200).json({message: 'Updated!'})
+})
+
 //Start this app
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`)
