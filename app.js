544a7244ecbd6fdc7f1647f96ee7291255dbe1b1
LuisCasillasA <launchx10359@innovaccion.mx>
2022-05-09 12:39:28 -0500

    Delete request

--- app.js
+++ app.js
@@ -26,6 +26,13 @@ app.put('/v1/explorers/:id', (req, res) => {
     res.status(200).json({message: 'Updated!'})
 })
 
+app.delete('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers DELETE request ${new Date()}`)
+    console.log(`Delete explorer with id ${req.params.id}`)
+    const requestBody = req.body //Params client 
+    res.status(200).json({message: 'Deleted'})
+})
+
 //Start this app
 app.listen(port, () =>{
     console.log(`Example app listening on port ${port}`)
